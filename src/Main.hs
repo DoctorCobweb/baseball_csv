cdb1cc96bf331cfdc4c7ac97ee2a0965a43f9cc7
DoctorCobweb <spinninghalf@gmail.com>
2015-01-30 16:04:27 +1100

    explain some things

--- src/Main.hs
+++ src/Main.hs
@@ -12,6 +12,12 @@ main :: IO ()
 main = do
   csvData <- BL.readFile "batting.csv"
   let v = decode NoHeader csvData :: Either String (V.Vector BaseballStats)
+
+  -- if csv decode works, then we will have a Right (V.Vector BaseballStats) value for
+  -- Either. 
+  -- in this case, fmap will be able to apply the function (V.foldr summer 0) to it.
+  -- since v is a vector of BaseballStats the foldr can reduce the elements down using
+  -- the starting value of 0 and the summer function.
   let summed = fmap (V.foldr summer 0) v
   putStrLn $ "Total atBats was: " ++ (show summed)
   where summer (name, year, team, atBats) n = n + atBats
